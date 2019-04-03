0d5a1427c017a0c5d8abc1c75e19c8d1cdc8dc99
Jinqiu Yu <yujinqiuustb@gmail.com>
2019-04-03 19:20:08 +0800

    Initial Commit

---
+++ SwiftBridge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftBridge
+//
+//  Created by knight on 4/3/19.
+//  Copyright © 2019 JimYu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
